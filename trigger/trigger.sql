bb2475111d44487504a95f9b50147af6aacdaffe
HWK\hyunmin.song <hyunmin.song@bespinglobal.com>
2023-08-24 15:09:23 +0900

    변수 이름 변경

--- trigger/trigger.sql
+++ trigger/trigger.sql
@@ -7,10 +7,11 @@ BEGIN
 	declare @sql nvarchar(MAX)
 	declare @columns NVARCHAR(MAX)
 	declare @values NVARCHAR(MAX);
-	declare @count int
-	declare @insertedCount int
-	declare @insertedMax int
-	declare @max int
+	declare @ColumCount int
+	declare @ColumCountMax int
+	declare @RowCount int
+	declare @RowCountMax int
+	
 
 	declare @result nvarchar(max)
 	declare @tmpName nvarchar(max) 
@@ -19,25 +20,23 @@ BEGIN
 	declare @param nvarchar(max) 
 	declare @tmpVal nvarchar(max)	
 	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	
-	DECLARE @currentSessionID INT;
+	-- interfering with SELECT statements.	
 	--SET NOCOUNT ON;
 
 	SET @TableName = 'test';
 	SET @columns = '';
 	SET @values = '';
-	SET @count = 1;
-	SET @max = 0;
-	SET @insertedCount = 1;
-	SET @insertedMax = 0;
+	SET @ColumCount = 1;
+	SET @ColumCountMax = 0;
+	SET @RowCount = 1;
+	SET @RowCountMax = 0;
 	 -- INSERT 작업인 경우
     IF EXISTS (SELECT * FROM INSERTED) AND NOT EXISTS (SELECT * FROM DELETED)
     BEGIN
 
 		-- current table colums information 
 		select *, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rn into #rp_inserted from inserted;
-		select @insertedMax = count(*) from #rp_inserted;
+		select @RowCountMax = count(*) from #rp_inserted;
 
 		SELECT @columns = @columns + column_name + ','
 		FROM information_schema.columns
@@ -48,21 +47,21 @@ BEGIN
 		FROM information_schema.columns
 		WHERE table_name = @TableName;
 
-		select @max = count(*) from #tempColumnTables;
+		select @ColumCountMax = count(*) from #tempColumnTables;
 		SET @sql = 'insert into ' +@TableName+' ('+@columns + ') '
 		SET @sql = @sql + ' values';
 
 	
-		while @insertedCount <= @insertedMax
+		while @RowCount <= @RowCountMax
 		begin 
-			SET @count = 1;
+			SET @ColumCount = 1;
 			
-			while @count <= @max
+			while @ColumCount <= @ColumCountMax
 			begin
 							
-				set @tmpName = ( select name from #tempColumnTables where id = @count );		
-				set @tmpType = (select type from #tempColumnTables where id = @count);
-				set @tmpQuery = N'select @result = '+@tmpName+' from #rp_inserted where rn =' + CAST(@insertedCount AS nvarchar);
+				set @tmpName = ( select name from #tempColumnTables where id = @ColumCount );		
+				set @tmpType = (select type from #tempColumnTables where id = @ColumCount);
+				set @tmpQuery = N'select @result = '+@tmpName+' from #rp_inserted where rn =' + CAST(@RowCount AS nvarchar);
 				set @param = N'@result nvarchar(max) OUTPUT';						
 				set @tmpVal = '';
 				EXEC sp_executesql @tmpQuery, @param, @result=@tmpVal OUTPUT;
@@ -76,7 +75,7 @@ BEGIN
 					set @values = @values + @tmpVal + ','
 				end
 
-				set @count = @count + 1;
+				set @ColumCount = @ColumCount + 1;
 				
 			end;
 			SET @values = LEFT(@values, LEN(@values) - 1);
@@ -84,7 +83,7 @@ BEGIN
 			-- make dynamic sql
 			SET @sql = @sql + ' ('+@values+'),';
 			SET @values = ''
-			SET @insertedCount = @insertedCount + 1;
+			SET @RowCount = @RowCount + 1;
 			
 		end
 
@@ -99,26 +98,26 @@ BEGIN
 		
 		-- current table colums information 
 		select distinct *, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rn into #rp_deleted from deleted;
-		select @insertedMax = count(*) from #rp_deleted;
+		select @RowCountMax = count(*) from #rp_deleted;
 
 		SELECT ORDINAL_POSITION as id , column_name as name, DATA_TYPE as type  into #tempDelColumnTables
 		FROM information_schema.columns
 		WHERE table_name = @TableName;
 
-		select @max = count(*) from #tempDelColumnTables;
+		select @ColumCountMax = count(*) from #tempDelColumnTables;
 		
-		while @insertedCount <= @insertedMax
+		while @RowCount <= @RowCountMax
 		begin 
-			SET @count = 1;
+			SET @ColumCount = 1;
 			SET @sql = 'delete ' +@TableName
 			SET @sql = @sql + ' where';
 
-			while @count <= @max
+			while @ColumCount <= @ColumCountMax
 			begin
 							
-				set @tmpName = ( select name from #tempDelColumnTables where id = @count );		
-				set @tmpType = (select type from #tempDelColumnTables where id = @count);
-				set @tmpQuery = N'select @result = ' +@tmpName+ ' from #rp_deleted where rn =' + CAST(@insertedCount AS nvarchar);
+				set @tmpName = ( select name from #tempDelColumnTables where id = @ColumCount );		
+				set @tmpType = (select type from #tempDelColumnTables where id = @ColumCount);
+				set @tmpQuery = N'select @result = ' +@tmpName+ ' from #rp_deleted where rn =' + CAST(@RowCount AS nvarchar);
 				set @param = N'@result nvarchar(max) OUTPUT';						
 				set @tmpVal = '';
 				EXEC sp_executesql @tmpQuery, @param, @result=@tmpVal OUTPUT;
@@ -133,7 +132,7 @@ BEGIN
 					set @values = @values +' '+ @tmpName + '=' + @tmpVal + ' and'
 				end
 				
-				set @count = @count + 1;
+				set @ColumCount = @ColumCount + 1;
 				
 			end;
 
@@ -144,7 +143,7 @@ BEGIN
 			SET @sql =@sql + ';';
 			-- insert history db query		
 			insert into history values(1,@sql,2);
-			SET @insertedCount = @insertedCount + 1;
+			SET @RowCount = @RowCount + 1;
 			
 		end
 
@@ -158,38 +157,38 @@ BEGIN
 		select distinct *, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rn into #up_deleted from deleted;
 		select distinct *, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rn into #up_inserted from inserted;
 		
-		select @insertedMax = count(*) from #up_deleted;
+		select @RowCountMax = count(*) from #up_deleted;
 
 		SELECT ORDINAL_POSITION as id , column_name as name, DATA_TYPE as type  into #tempUpColumnTables
 		FROM information_schema.columns
 		WHERE table_name = @TableName;
 
 
-		select @max = count(*) from #tempUpColumnTables;
+		select @ColumCountMax = count(*) from #tempUpColumnTables;
 
 
-		while @insertedCount <= @insertedMax
+		while @RowCount <= @RowCountMax
 		begin 
-			SET @count = 1;
+			SET @ColumCount = 1;
 			SET @sql = 'update ' +@TableName			
 			declare @setString nvarchar(max) = ' set'
 			declare @whereString nvarchar(max) = ' where'
 			declare @tmpOldVal nvarchar(max) ='';
 			declare @tmpNewVal nvarchar(max) ='';
 
-			while @count <= @max
+			while @ColumCount <= @ColumCountMax
 			begin
 							
-				set @tmpName = ( select name from #tempUpColumnTables where id = @count );		
-				set @tmpType = (select type from #tempUpColumnTables where id = @count);
+				set @tmpName = ( select name from #tempUpColumnTables where id = @ColumCount );		
+				set @tmpType = (select type from #tempUpColumnTables where id = @ColumCount);
 				
-				set @tmpQuery = N'select @result = ' +@tmpName+ ' from #up_deleted where rn =' + CAST(@insertedCount AS nvarchar);
+				set @tmpQuery = N'select @result = ' +@tmpName+ ' from #up_deleted where rn =' + CAST(@RowCount AS nvarchar);
 				set @param = N'@result nvarchar(max) OUTPUT';						
 				set @tmpOldVal = '';
 				EXEC sp_executesql @tmpQuery, @param, @result=@tmpOldVal OUTPUT;
 
 
-				set @tmpQuery = N'select @result = ' +@tmpName+ ' from #up_inserted where rn =' + CAST(@insertedCount AS nvarchar);
+				set @tmpQuery = N'select @result = ' +@tmpName+ ' from #up_inserted where rn =' + CAST(@RowCount AS nvarchar);
 				set @param = N'@result nvarchar(max) OUTPUT';						
 				set @tmpNewVal = '';
 				EXEC sp_executesql @tmpQuery, @param, @result=@tmpNewVal OUTPUT;
@@ -206,7 +205,7 @@ BEGIN
 					set @whereString = @whereString +' '+ @tmpName + '=' + @tmpOldVal + ' and'
 				end
 				
-				set @count = @count + 1;
+				set @ColumCount = @ColumCount + 1;
 				
 			end;
 
@@ -218,7 +217,7 @@ BEGIN
 			SET @sql =@sql + ';';
 			-- insert history db query		
 			insert into history values(1,@sql,3);
-			SET @insertedCount = @insertedCount + 1;
+			SET @RowCount = @RowCount + 1;
 			
 		end
 
